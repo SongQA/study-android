4b7af02d4c0e627b26e3bf77a37e700ab754bb87
eck.Song <eck.song92@rainist.com>
2022-04-15 01:19:11 +0900

    Add Song Todolist

--- app/src/main/java/com/example/study_kotlin/activity_song.kt
+++ app/src/main/java/com/example/study_kotlin/activity_song.kt
@@ -2,10 +2,33 @@ package com.example.study_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.study_kotlin.song.Todo
+import com.example.study_kotlin.song.TodoAdaptor
+import kotlinx.android.synthetic.main.activity_song.*
 
 class activity_song : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdaptor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_song)
+        todoAdapter = TodoAdaptor(mutableListOf())
+
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+            if(todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+        btnDeleteDoneTodos.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
     }
 }
