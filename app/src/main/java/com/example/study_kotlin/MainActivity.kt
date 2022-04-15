b5e8779c60b78691b522b0de3ed644e1bebc47b7
minjaeim <minjaeim@rainist.com>
2022-04-15 20:00:17 +0900

    changes : lotto complete

--- app/src/main/java/com/example/study_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/study_kotlin/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/study_kotlin/activity_lym.kt
+++ app/src/main/java/com/example/study_kotlin/activity_lym.kt
@@ -1,11 +1,27 @@
 package com.example.study_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import kotlinx.android.synthetic.main.activity_lym.*
 
 class activity_lym : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lym)
+
+        animationview.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        } // 클릭 시에는 MainActivty 로 이동
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, mjMainActivity_lym::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3초 대기 시 mjMainActivity 로 이동
     }
 }
