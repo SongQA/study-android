ca9d4c14fc1030621a2004464d063b09f0915a30
Jaeeun Ahn <97071920+jaeeuna@users.noreply.github.com>
2022-04-22 16:24:26 +0900

    Revert "[CREATE] create to-do app layout & set up simple code"

---
+++ app/src/main/java/com/example/study_kotlin/activity_ahn1.kt
@@ -0,0 +1,11 @@
+package com.example.study_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_ahn1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ahn1)
+    }
+}
