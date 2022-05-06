c4d4937386c74ee68a818cdddddf87323295e4e7
juhee11 <ahnjuhee@rainist.com>
2022-05-06 15:03:59 +0900

    Create ToDoAdapter.kr

--- app/src/main/java/com/example/study_kotlin/activity_ahn2.kt
+++ app/src/main/java/com/example/study_kotlin/activity_ahn2.kt
@@ -1,22 +1,35 @@
 package com.example.study_kotlin
 
-import android.content.ContentValues
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.study_kotlin.juhee.Todo
+import com.example.study_kotlin.juhee.TodoAdapter
 import kotlinx.android.synthetic.main.activity_ahn2.*
 
 class activity_ahn2 : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ahn2)
+        todoAdapter = TodoAdapter(mutableListOf())
 
-        fb_add.setOnClickListener {
-            val intentAdd = Intent(this, Activity_add::class.java)
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
 
-            Log.i(ContentValues.TAG, "Intent")
-            startActivity(intentAdd)
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+            if(todoTitle.isNotEmpty()){
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+
+            }
+        }
+        btnDeleteDoneTodos.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
         }
     }
 }
